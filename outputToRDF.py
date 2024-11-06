79379085b07e2da08b649c31163c03859a65ad3a
Binderová Alice <56399637+bindeali@users.noreply.github.com>
2024-11-06 16:36:14 +0100

    XLSX to OFN support draft

--- outputToRDF.py
+++ outputToRDF.py
@@ -2,12 +2,14 @@ from rdflib import XSD, Graph, URIRef, Literal, BNode
 from rdflib.namespace import RDF, OWL, RDFS, SKOS, DCTERMS, TIME
 from ofnClasses import ClassType, RPPType, Relationship, Term, TermClass, Trope, VocabularyType, Vocabulary
 from datetime import datetime
+from urllib.parse import urlparse
 
 # TODO: Better IRI generation (PN_LOCAL)
 # TODO: Code lists
 # TODO: Error handling
 # TODO: Input validation
-
+# TODO: check for duplicate values
+# TODO: search for terms within file is IRI is not provided
 # TODO: Write to file
 
 
@@ -15,6 +17,19 @@ def getRDFoutput(graph: Graph, outputLocation: str):
     with open(outputLocation, "w", encoding="utf-8") as outputFile:
         format: str = outputLocation[len(outputLocation) - 3:]
         outputFile.write(graph.serialize(format=format))
+
+
+def testInputString(string: str) -> bool:
+    return string is not None and string.strip() != ""
+
+
+def getURIRefOrLiteral(string: str):
+    result = URIRef(string)
+    try:
+        result.n3()
+        return result
+    except Exception:
+        return Literal(string)
 
 
 def convertToRDF(vocabulary: Vocabulary, defaultLanguage: str, outputFile: str):
@@ -50,22 +65,30 @@ def convertToRDF(vocabulary: Vocabulary, defaultLanguage: str, outputFile: str):
         graph.add((termIRI, SKOS.inScheme, vocabularyIRI))
         # prefLabel
         for lang, name in term.name.items():
-            graph.add((termIRI, SKOS.prefLabel, Literal(name, lang)))
+            if testInputString(name):
+                graph.add((termIRI, SKOS.prefLabel, Literal(name, lang)))
         # description
         for lang, name in term.description.items():
-            graph.add((termIRI, DCTERMS.description, Literal(name, lang)))
+            if testInputString(name):
+                graph.add((termIRI, DCTERMS.description, Literal(name, lang)))
         # definition
         for lang, name in term.definition.items():
-            graph.add((termIRI, DCTERMS.definition, Literal(name, lang)))
+            if testInputString(name):
+                graph.add((termIRI, SKOS.definition, Literal(name, lang)))
         # relation
         for relation in term.related:
-            graph.add((termIRI, DCTERMS.relation, URIRef(relation)))
+            if testInputString(relation):
+                graph.add((termIRI, DCTERMS.relation,
+                          getURIRefOrLiteral(relation)))
         # conformsTo
-        if term.source is not None:
-            graph.add((termIRI, DCTERMS.conformsTo, URIRef(term.source)))
+        if testInputString(term.source):
+            graph.add((termIRI, DCTERMS.conformsTo,
+                      getURIRefOrLiteral(term.source)))
         # exactMatch
         for equivalent in term.equivalent:
-            graph.add((termIRI, SKOS.exactMatch, URIRef(equivalent)))
+            if testInputString(equivalent):
+                graph.add((termIRI, SKOS.exactMatch,
+                          getURIRefOrLiteral(equivalent)))
         # shared in PPDF
         if term.sharedInPPDF is not None:
             graph.add((termIRI, URIRef("https://slovník.gov.cz/agendový/104/pojem/je-sdílen-v-propojeném-datovém-fondu"), Literal(
@@ -74,17 +97,21 @@ def convertToRDF(vocabulary: Vocabulary, defaultLanguage: str, outputFile: str):
         # term types (& broader)
         if isinstance(term, Term):
             for broader in term.subClassOf:
-                graph.add((termIRI, SKOS.broader, URIRef(broader)))
+                if testInputString(broader):
+                    graph.add((termIRI, SKOS.broader,
+                              getURIRefOrLiteral(broader)))
         if isinstance(term, TermClass):
             graph.add((termIRI, RDF.type, OWL.Class))
-            if term.ais is not None:
+            if testInputString(term.ais):
                 graph.add((termIRI, URIRef(
-                    "https://slovník.gov.cz/agendový/104/pojem/údaje-jsou-v-ais"), URIRef(term.ais)))
-            if term.agenda is not None:
+                    "https://slovník.gov.cz/agendový/104/pojem/údaje-jsou-v-ais"), getURIRefOrLiteral(term.ais)))
+            if testInputString(term.agenda):
                 graph.add((termIRI, URIRef(
-                    "https://slovník.gov.cz/agendový/104/pojem/sdružuje-údaje-vedené-nebo-vytvářené-v-rámci-agendy"), URIRef(term.agenda)))
+                    "https://slovník.gov.cz/agendový/104/pojem/sdružuje-údaje-vedené-nebo-vytvářené-v-rámci-agendy"), getURIRefOrLiteral(term.agenda)))
             for broader in term.subClassOf:
-                graph.add((termIRI, RDFS.subClassOf, URIRef(broader)))
+                if testInputString(broader):
+                    graph.add((termIRI, RDFS.subClassOf,
+                              getURIRefOrLiteral(broader)))
         if isinstance(term, TermClass) and term.type is ClassType.OBJECT:
             graph.add((termIRI, RDF.type, URIRef(
                 "https://slovník.gov.cz/veřejný-sektor/pojem/typ-objektu-práva")))
@@ -93,19 +120,24 @@ def convertToRDF(vocabulary: Vocabulary, defaultLanguage: str, outputFile: str):
                 "https://slovník.gov.cz/veřejný-sektor/pojem/typ-subjektu-práva")))
         if isinstance(term, Trope) or isinstance(term, Relationship):
             for broader in term.subClassOf:
-                graph.add((termIRI, RDFS.subPropertyOf, URIRef(broader)))
+                if testInputString(broader):
+                    graph.add((termIRI, RDFS.subPropertyOf,
+                              getURIRefOrLiteral(broader)))
         if isinstance(term, Trope):
             graph.add((termIRI, RDF.type, OWL.DatatypeProperty))
             if term.target is not None:
-                graph.add((termIRI, RDFS.domain, URIRef(term.target)))
+                graph.add(
+                    (termIRI, RDFS.domain, getURIRefOrLiteral(term.target)))
             if term.datatype is not None:
-                graph.add((termIRI, RDFS.range, URIRef(term.datatype)))
+                graph.add(
+                    (termIRI, RDFS.range, getURIRefOrLiteral(term.datatype)))
         if isinstance(term, Relationship):
             graph.add((termIRI, RDF.type, OWL.ObjectProperty))
             if term.domain is not None:
-                graph.add((termIRI, RDFS.domain, URIRef(term.domain)))
+                graph.add(
+                    (termIRI, RDFS.domain, getURIRefOrLiteral(term.domain)))
             if term.range is not None:
-                graph.add((termIRI, RDFS.range, URIRef(term.range)))
+                graph.add((termIRI, RDFS.range, getURIRefOrLiteral(term.range)))
 
         # RPP public/private types
         if term.rppType is not None:
@@ -118,4 +150,4 @@ def convertToRDF(vocabulary: Vocabulary, defaultLanguage: str, outputFile: str):
                 graph.add((termIRI, RDF.type, URIRef(
                     term.rppPrivateTypeSource)))
 
-        getRDFoutput(graph, outputFile)
+    getRDFoutput(graph, outputFile)
---
+++ tableToOFN.py
@@ -0,0 +1,250 @@
+from typing import List
+import openpyxl
+import sys
+from ofnClasses import Relationship, TermClass, Trope, Vocabulary, VocabularyType, getClass, getTrope, ClassType
+from outputToRDF import convertToRDF
+
+inputLocation = sys.argv[1]
+outputLocation = sys.argv[2]
+defaultLanguage = "cs"
+
+# TODO: CSV support
+
+
+def xlsxToSheets(file: str):
+    soSheet = None
+    itSheet = None
+    rlSheet = None
+    if file.endswith("xlsx"):
+        wb = openpyxl.load_workbook(file, data_only=True)
+        soSheet = wb["Subjekty a objekty práva"]
+        itSheet = wb["Vlastnosti"]
+        rlSheet = wb["Vztahy"]
+
+    if soSheet is not None and itSheet is not None and rlSheet is not None:
+        return (soSheet, itSheet, rlSheet)
+    else:
+        raise Exception()
+
+
+def soSheetToOFN(sheet) -> List[TermClass]:
+    header = True
+    nameIndex = None
+    descriptionIndex = None
+    definitionIndex = None
+    sourceIndex = None
+    subClassOfIndex = None
+    equivalentIndex = None
+    aisIndex = None
+    agendaIndex = None
+    iriIndex = None
+    typeIndex = None
+    list = []
+    for lst in sheet:
+        row = [cell.value for cell in lst]
+        if header:
+            nameIndex = row.index("Název")
+            descriptionIndex = row.index("Popis")
+            definitionIndex = row.index("Definice")
+            sourceIndex = row.index("Zdroj")
+            subClassOfIndex = row.index("Nadřazený pojem")
+            equivalentIndex = row.index("Ekvivalentní pojem")
+            iriIndex = row.index("Identifikátor")
+            aisIndex = row.index("Agendový informační systém")
+            agendaIndex = row.index("Agenda")
+            typeIndex = row.index("Typ")
+            header = False
+        else:
+            term = TermClass()
+            if row[nameIndex] is None:
+                continue
+            term.name = {defaultLanguage: row[nameIndex]}
+            if (row[typeIndex]):
+                if row[typeIndex].strip().lower() == "subjekt práva":
+                    term.type = ClassType.SUBJECT
+                elif row[typeIndex].strip().lower() == "objekt práva":
+                    term.type = ClassType.OBJECT
+            if row[definitionIndex]:
+                term.definition = {defaultLanguage: row[definitionIndex]}
+            if row[descriptionIndex]:
+                term.description = {defaultLanguage: row[descriptionIndex]}
+            if row[sourceIndex]:
+                term.source = row[sourceIndex]
+            if row[subClassOfIndex]:
+                term.subClassOf.append(row[subClassOfIndex])
+            if row[equivalentIndex]:
+                term.equivalent.append(row[equivalentIndex])
+            if row[iriIndex]:
+                term.iri = row[iriIndex]
+            if row[aisIndex]:
+                term.ais = row[aisIndex]
+            if row[agendaIndex]:
+                term.agenda = row[agendaIndex]
+            list.append(term)
+    return list
+
+
+def itSheetToOFN(sheet) -> List[Trope]:
+    header = True
+    termClassIndex = None
+    datatypeIndex = None
+    nameIndex = None
+    descriptionIndex = None
+    definitionIndex = None
+    sourceIndex = None
+    subClassOfIndex = None
+    equivalentIndex = None
+    sharedInPpdfIndex = None
+    rppTypeIndex = None
+    rppPrivateTypeSourceIndex = None
+    iriIndex = None
+    list = []
+    for lst in sheet:
+        row = [cell.value for cell in lst]
+        if header:
+            nameIndex = row.index("Název")
+            descriptionIndex = row.index("Popis")
+            definitionIndex = row.index("Definice")
+            sourceIndex = row.index("Zdroj")
+            subClassOfIndex = row.index("Nadřazený pojem")
+            equivalentIndex = row.index("Ekvivalentní pojem")
+            iriIndex = row.index("Identifikátor")
+            termClassIndex = row.index("Subjekt nebo objekt práva")
+            datatypeIndex = row.index("Datový typ")
+            sharedInPpdfIndex = row.index("Je pojem sdílen v PPDF?")
+            rppTypeIndex = row.index("Je pojem veřejný?")
+            rppPrivateTypeSourceIndex = row.index(
+                "Ustanovení dokládající neveřejnost pojmu")
+            header = False
+        else:
+            if row[nameIndex] is None or row[termClassIndex] is None:
+                continue
+            term = Trope()
+            term.name = {defaultLanguage: row[nameIndex]}
+            term.target = row[termClassIndex]
+            if row[definitionIndex]:
+                term.definition = {defaultLanguage: row[definitionIndex]}
+            if row[descriptionIndex]:
+                term.description = {defaultLanguage: row[descriptionIndex]}
+            if row[sourceIndex]:
+                term.source = row[sourceIndex]
+            if row[subClassOfIndex]:
+                term.subClassOf.append(row[subClassOfIndex])
+            if row[equivalentIndex]:
+                term.equivalent.append(row[equivalentIndex])
+            if row[iriIndex]:
+                term.iri = row[iriIndex]
+            if row[sharedInPpdfIndex]:
+                term.sharedInPPDF = row[sharedInPpdfIndex]
+            if row[datatypeIndex]:
+                term.datatype = row[datatypeIndex]
+            if row[rppTypeIndex]:
+                term.rppType = row[rppTypeIndex]
+            if row[rppPrivateTypeSourceIndex]:
+                term.rppPrivateTypeSource = row[rppPrivateTypeSourceIndex]
+            list.append(term)
+    return list
+
+
+def rlSheetToOFN(sheet) -> List[Relationship]:
+    header = True
+    termClassSourceIndex = None
+    termClassTargetIndex = None
+    nameIndex = None
+    descriptionIndex = None
+    definitionIndex = None
+    sourceIndex = None
+    subClassOfIndex = None
+    equivalentIndex = None
+    iriIndex = None
+    list = []
+    for lst in sheet:
+        row = [cell.value for cell in lst]
+        if header:
+            nameIndex = row.index("Název")
+            descriptionIndex = row.index("Popis")
+            definitionIndex = row.index("Definice")
+            sourceIndex = row.index("Zdroj")
+            subClassOfIndex = row.index("Nadřazený pojem")
+            equivalentIndex = row.index("Ekvivalentní pojem")
+            iriIndex = row.index("Identifikátor")
+            termClassIndices = [x for x, y in enumerate(
+                row) if y == "Subjekt nebo objekt práva"]
+            termClassSourceIndex = termClassIndices[0]
+            termClassTargetIndex = termClassIndices[1]
+            header = False
+        else:
+            if row[nameIndex] is None or row[termClassSourceIndex] is None or row[termClassTargetIndex] is None:
+                continue
+            term = Relationship(row[termClassSourceIndex],
+                                row[termClassTargetIndex])
+            term.name = {defaultLanguage: row[nameIndex]}
+            if row[definitionIndex]:
+                term.definition = {defaultLanguage: row[definitionIndex]}
+            if row[descriptionIndex]:
+                term.description = {defaultLanguage: row[descriptionIndex]}
+            if row[sourceIndex]:
+                term.source = row[sourceIndex]
+            if row[subClassOfIndex]:
+                term.subClassOf.append(row[subClassOfIndex])
+            if row[equivalentIndex]:
+                term.equivalent.append(row[equivalentIndex])
+            if row[iriIndex]:
+                term.iri = row[iriIndex]
+            list.append(term)
+    return list
+
+
+def tableToOFN():
+    (soSheet, itSheet, rlSheet) = xlsxToSheets(inputLocation)
+    soList = soSheetToOFN(soSheet)
+    itList = itSheetToOFN(itSheet)
+    rlList = rlSheetToOFN(rlSheet)
+    vocabulary = Vocabulary()
+    vocabulary.terms.extend(soList)
+    vocabulary.terms.extend(itList)
+    vocabulary.terms.extend(rlList)
+    if len(itList) > 0 or len(rlList) > 0:
+        vocabulary.type = VocabularyType.CONCEPTUAL_MODEL
+
+    vocabulary.name = {defaultLanguage: "test"}
+    convertToRDF(vocabulary, defaultLanguage, outputLocation)
+
+
+tableToOFN()
+
+# def tableToOFN(soSheet, itSheet, rlSheet):
+
+#     convertToRDF(vocabulary, defaultLanguage, outputLocation)
+
+# def csvToSheets(soFile: str, itFile: str, rlFile: str):
+#     soExtension = os.path.splitext(soFile)
+# itExtension = os.path.splitext(itFile)
+# rlExtension = os.path.splitext(rlFile)
+# soSheet = None
+# itSheet = None
+# rlSheet = None
+# if "csv" in soExtension[1]:
+#     with open(soFile) as soCSV:
+#         for row in soCSV:
+#             print([cell for cell in row])
+# if itExtension is "csv":
+#     pass
+# if rlExtension is "csv":
+#     pass
+
+# if soSheet is not None and itSheet is not None and rlSheet is not None:
+#     return (soSheet, itSheet, rlSheet)
+# else:
+#     raise Exception()
+# wb = openpyxl.load_workbook(file, read_only=True, data_only=True)
+# soSheet = wb["Subjekty a objekty práva"]
+# itSheet = wb["Vlastnosti"]
+# rlSheet = wb["Vztahy"]
+# if (soSheet is not None):
+#     for row in soSheet.iter_rows():
+#         print([cell.value for cell in row])
+# if (itSheet is not None):
+#     pass
+# if (rlSheet is not None):
+#     pass
