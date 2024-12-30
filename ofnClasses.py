8876d10cf434247e4919c564158877f1ed82607e
bindeali <56399637+bindeali@users.noreply.github.com>
2024-12-30 11:08:58 +0100

    feat: implement PN_LOCAL regex

--- ofnClasses.py
+++ ofnClasses.py
@@ -1,10 +1,29 @@
 from enum import Enum
+import re
 
 ClassType = Enum("ClassType", ['CLASS', 'SUBJECT', 'OBJECT'])
 
 VocabularyType = Enum("VocabularyType", ["THESAURUS", "CONCEPTUAL_MODEL"])
 
 RPPType = Enum("RPPType", ["PUBLIC", "PRIVATE"])
+
+# https://www.w3.org/TR/sparql11-query/#rPN_LOCAL
+PN_CHARS_U = re.compile(
+    "[A-Za-z]|[\u00C0-\u00D6]|[\u00D8-\u00F6]|[\u00F8-\u02FF]|[\u0370-\u037D]|[\u037F-\u1FFF]|[\u200C-\u200D]|[\u2070-\u218F]|[\u2C00-\u2FEF]|[\u3001-\uD7FF]|[\uF900-\uFDCF]|[\uFDF0-\uFFFD]|[\U00010000-\U000EFFFF]|_", re.U)
+PERCENT = re.compile("%([0-9A-Fa-f])([0-9A-Fa-f])", re.U)
+PN_LOCAL_ESC = re.compile("\\\\[_~\\.\\-!$&\"'()*+,;=/?#@%]")
+PLX = re.compile("({})|({})".format(
+    PERCENT.pattern, PN_LOCAL_ESC.pattern), re.I)
+PN_CHARS = re.compile(
+    "({})|[-0-9]|\u00B7|[\u0300-\u036F]|[\u203F-\u2040]".format(PN_CHARS_U.pattern), re.U)
+PN_LOCAL_1 = re.compile(
+    "({})|[:0-9]|({})".format(PN_CHARS_U.pattern, PLX.pattern))
+PN_LOCAL_2 = re.compile("({})|[.:]|({})".format(
+    PN_CHARS.pattern, PLX.pattern))
+PN_LOCAL_3 = re.compile("({})|:|({})".format(
+    PN_CHARS.pattern, PLX.pattern))
+PN_LOCAL = re.compile("({})(({})*({}))?".format(PN_LOCAL_1.pattern,
+                                                PN_LOCAL_2.pattern, PN_LOCAL_3.pattern), re.U)
 
 
 class Resource:
@@ -21,12 +40,13 @@ class Resource:
 class Vocabulary(Resource):
     def __init__(self) -> None:
         super().__init__()
+        self.lkod: str = ""
         self.terms: list[Term] = []
         self.type: VocabularyType = VocabularyType.THESAURUS
 
     def getIRI(self, defaultLanguage: str) -> str:
-        self.iri = "https://slovník.gov.cz/{}".format(
-            self.name[defaultLanguage].strip().lower().replace(" ", "-"))
+        self.iri = "{}/{}".format(self.lkod,
+                                  self.name[defaultLanguage].strip().lower().replace(" ", "-"))
         return self.iri
 
 
@@ -45,9 +65,17 @@ class Term(Resource):
         self.rppPrivateTypeSource: str | None = None
 
     def getIRI(self, vocabulary: Vocabulary, defaultLanguage: str) -> str:
-        self.iri = "https://slovník.gov.cz/{}/pojem/{}".format(
-            vocabulary.name[defaultLanguage].strip().lower().replace(" ", "-"),
-            self.name[defaultLanguage].strip().lower().replace(" ", "-"))
+        name: str = self.name[defaultLanguage].strip().lower()
+        result: str = ""
+        for match in PN_LOCAL.finditer(name):
+            m = match.group(0)
+            result = result.ljust(match.end(0), "-")
+            result = result[0:(match.end(0) - len(m))]
+            result += m
+        result = re.sub("-+$", "", result)
+        self.iri = "{}/pojem/{}".format(
+            vocabulary.getIRI(),
+            result)
         return self.iri
 
 
